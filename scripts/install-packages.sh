680535028bfcc580a50aa5d5bb8bb334b18b1737
KumaTea <KumaTea@outlook.com>
2022-04-23 18:54:50 +0800

    add index arg

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -4,6 +4,7 @@ set -e
 
 PACKAGES=${1:-""}
 PIP_PACKAGES=${2:-""}
+CUSTOM_INDEX_URL=${3:-""}
 
 source /root/.bashrc > /dev/null
 CONDA_EXE=/opt/conda/bin/conda
@@ -16,7 +17,11 @@ fi
 if [ -n "$PIP_PACKAGES" ]; then
   echo "Installing pip packages: $PIP_PACKAGES"
   PYTHON_BIN=/opt/conda/bin/python3
-  $PYTHON_BIN -m pip install -U $PIP_PACKAGES
+  if [ -n "$CUSTOM_INDEX_URL" ]; then
+    $PYTHON_BIN -m pip install $PIP_PACKAGES -f $CUSTOM_INDEX_URL
+  else
+    $PYTHON_BIN -m pip install $PIP_PACKAGES
+  fi
 fi
 
 echo "Cleanup..."
