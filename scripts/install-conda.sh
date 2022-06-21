402b10f1f4eaf6dd1d0358aad5089e0c8400f0c1
KumaTea <KumaTea@outlook.com>
2022-06-22 03:01:09 +0800

    update scripts for deploying on cloud

--- scripts/install-conda.sh
+++ scripts/install-conda.sh
@@ -2,9 +2,11 @@
 
 set -e
 
+ARCH=$(uname -m)
+
 echo "Downloading Miniforge (conda) installer script..."
 # wget http://192.168.2.225:8080/Miniforge3-Linux-aarch64.sh -O /tmp/install-conda.sh
-wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-aarch64.sh -O /tmp/install-conda.sh
+wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-$ARCH.sh" -O /tmp/install-conda.sh
 
 echo "Installing conda..."
 bash /tmp/install-conda.sh -b -p /opt/conda
@@ -15,9 +17,9 @@ $CONDA_EXE init bash
 source /root/.bashrc > /dev/null
 $CONDA_EXE config --set show_channel_urls false
 
-echo "Setting PyPI mirror..."
-PYTHON_BIN=/opt/conda/bin/python3
-$PYTHON_BIN -m pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+# echo "Setting PyPI mirror..."
+# PYTHON_BIN=/opt/conda/bin/python3
+# $PYTHON_BIN -m pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 
 echo "Cleanup..."
 $CONDA_EXE clean --all -y
