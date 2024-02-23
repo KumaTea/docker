39b74168167dc25da182563befe79903e7f1e6ca
KumaTea <KumaTea@outlook.com>
2024-02-23 19:43:25 +0800

    update bot dockerfile

--- scripts/install-conda.sh
+++ scripts/install-conda.sh
@@ -5,8 +5,9 @@ set -e
 ARCH=$(uname -m)
 
 echo "Downloading Miniforge (conda) installer script..."
-# wget http://192.168.2.225:8080/Miniforge3-Linux-aarch64.sh -O /tmp/install-conda.sh
-wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-$ARCH.sh" -O /tmp/install-conda.sh
+URL="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-$ARCH.sh"
+URL="https://gh.kmtea.eu/$URL"
+wget "$URL" -O /tmp/install-conda.sh
 
 echo "Installing conda..."
 bash /tmp/install-conda.sh -b -p /opt/conda
@@ -17,9 +18,10 @@ $CONDA_EXE init bash
 source /root/.bashrc > /dev/null
 $CONDA_EXE config --set show_channel_urls false
 
-# echo "Setting PyPI mirror..."
-# PYTHON_BIN=/opt/conda/bin/python3
-# $PYTHON_BIN -m pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+echo "Setting PyPI mirror..."
+PYTHON_BIN=/opt/conda/bin/python3
+$PYTHON_BIN -m pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+$PYTHON_BIN -m pip config set global.extra-index-url https://ext.kmtea.eu/cdn
 
 echo "Cleanup..."
 $CONDA_EXE clean --all -y
